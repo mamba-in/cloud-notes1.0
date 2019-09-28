78bd4c0dccdd1c5447ce1b5f9b22fb837b0ad1fa
wanglei <1290898818@qq.com>
2019-09-28 23:36:03 +0800

    update notes

--- docs/.vuepress/sidebarMap.js
+++ docs/.vuepress/sidebarMap.js
@@ -1,6 +1,6 @@
 const sidebarMap = [
   { title: "文章", dirname: "article" },
-  { title: "面试", dirname: "interview" },
+  { title: "总结", dirname: "notes" },
 	{ title: "问题", dirname: "issues" },
 ]
 
